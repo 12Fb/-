0218d6976a9161ba894ef4d15655c37d5892dccc
natuoi <2897620657@qq.com>
2024-10-22 15:55:01 +0800

    增加传递闭包功能

--- utils/algorithm.js
+++ utils/algorithm.js
@@ -1,9 +1,27 @@
-export function warshall(matrix) {
-  let n = matrix.length
-  let result = Array.from({length: n}, (_, i) => Array.from(matrix[i]))
-  for(let k = 0; k < n; k++)
-    for(let i = 0; i < n; i++)
-      for(let j = 0; j < n; j++)
+export function warshall(n, graph) {
+  const result = {}
+  graph.forEach(({u, v}) => {
+	if(u === null || v === null) return;
+    if(!result[u]) result[u] = {}
+    if(!result[v]) result[v] = {}
+    result[u][v] = 1
+  })
+  const nodes = Object.keys(result)
+  if(n < nodes.length) {
+	throw new Error("节点数量异常")
+  }
+  for(const x of nodes)
+    for(const y of nodes)
+      if(!result[x][y]) result[x][y] = 0
+  for(const k of nodes)
+    for(const i of nodes)
+      for(const j of nodes)
         result[i][j] = result[i][j] || (result[i][k] && result[k][j])
-  return result
+  let s = '{'
+  for(const x of nodes)
+    for(const y of nodes)
+	  if(result[x][y])
+	    s += `<${x},${y}>,`
+  s += '}'
+  return s
 }
