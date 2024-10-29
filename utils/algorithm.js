515f2cacee1d2810c9712681a086f9135db7e243
natuoi <2897620657@qq.com>
2024-10-29 12:06:22 +0800

    修改传递闭包功能

--- utils/algorithm.js
+++ utils/algorithm.js
@@ -1,27 +1,15 @@
-export function warshall(n, graph) {
-  const result = {}
-  graph.forEach(({u, v}) => {
-	if(u === null || v === null) return;
-    if(!result[u]) result[u] = {}
-    if(!result[v]) result[v] = {}
-    result[u][v] = 1
-  })
-  const nodes = Object.keys(result)
-  if(n < nodes.length) {
-	throw new Error("节点数量异常")
+export function warshall(graph) {
+  const n = graph.length
+  let result = [];
+  result.push(graph.map(row => row.slice()));
+  // Warshall 算法
+  for(let k = 0; k < n; k++) {
+	for(let i = 0; i < n; i++) {
+	  for(let j = 0; j < n; j++) {
+		graph[i][j] |= graph[i][k] * graph[k][j]
+	  }
+	}
+	result.push(graph.map(row => row.slice()))
   }
-  for(const x of nodes)
-    for(const y of nodes)
-      if(!result[x][y]) result[x][y] = 0
-  for(const k of nodes)
-    for(const i of nodes)
-      for(const j of nodes)
-        result[i][j] = result[i][j] || (result[i][k] && result[k][j])
-  let s = '{'
-  for(const x of nodes)
-    for(const y of nodes)
-	  if(result[x][y])
-	    s += `<${x},${y}>,`
-  s += '}'
-  return s
+  return result
 }
