96cc094761b0051e7b7950273e21480f199dec77
黄博远 <david.2003@foxmail.com>
2025-03-07 14:11:59 +0800

    feat: tabbar换回原生，不然遮罩层没法覆盖

--- pages/firstPage/components/card/index.js
+++ pages/firstPage/components/card/index.js
@@ -24,18 +24,27 @@ Component({
       ],
     }
   },
-
+  
   /**
    * 组件的初始数据
    */
   data: {
-
+    show:false
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-
+    onClose(){
+      this.setData({
+        show:false
+      })
+    },
+    intoPopup(){
+      this.setData({
+        show:true
+      })
+    }
   }
 })
